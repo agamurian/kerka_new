17f66036cb308b88483d23268ac936895f40ff49
Andrey Golovin <agamurian@gmail.com>
2024-03-12 02:41:50 +0100

    fixing look

---
+++ src/routes/events/+layout.svelte
@@ -0,0 +1,20 @@
+<div class="content">
+	<slot />
+</div>
+
+<style>
+	.content {
+		padding-left: 60px;
+  }
+  :global(img){
+    padding: 1rem;
+    max-width: 100vw;
+    max-height: 90vh;
+  }
+  :global(p){
+    display: inline-block;
+    padding: 2rem;
+    line-height: 1.5;
+    color: #111;
+  }
+</style>
