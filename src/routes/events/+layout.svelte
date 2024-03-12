5e6653218ffabcc5bd04bc8587d68699fd59bc2c
Andrey Golovin <agamurian@gmail.com>
2024-03-12 03:39:15 +0100

    styling

--- src/routes/events/+layout.svelte
+++ src/routes/events/+layout.svelte
@@ -7,14 +7,24 @@
 		padding-left: 60px;
   }
   :global(img){
-    padding: 1rem;
-    max-width: 100vw;
+    padding: 0rem;
+    max-width: calc(100vw - 4rem - 120px);
     max-height: 90vh;
   }
   :global(p){
+    max-width: calc(100vw - 120px - 4rem);
     display: inline-block;
-    padding: 2rem;
+    padding: 3vw;
     line-height: 1.5;
     color: #111;
+  }
+  :global(hr){
+    outline: none;
+      border: none;
+  border-top: 3px double #333;
+  color: #333;
+  overflow: visible;
+  text-align: center;
+  height: 5px;
   }
 </style>
